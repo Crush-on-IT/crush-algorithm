6430f5afda4a7c66b45a036fa30eb6a4752aaeb0
sumin416 <sumin416@naver.com>
2021-02-04 03:42:46 +0900

    Add file

--- src/su/DailyAlgo/SWEA2001_파리퇴치.java
+++ src/su/DailyAlgo/SWEA2001_파리퇴치.java
@@ -1,10 +1,50 @@
-package su.DailyAlgo;
+package src.su.DailyAlgo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class SWEA2001_파리퇴치 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			int answer = 0;
+			
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			
+			int[][] map = new int[N][N];
+			
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			for (int i = 0; i <= N-M; i++) {
+				for (int j = 0; j <= N-M; j++) {
+					int cur = 0;
+					for (int k = 0; k < M; k++) {
+						for (int k2 = 0; k2 < M; k2++) {
+							cur+=map[i+k][j+k2];
+						}
+					}
+					answer = Math.max(answer, cur);
+				}
+			}
+			
+			System.out.println("#"+t+" "+answer);
+		}
+		
+	} 
 
 }
---
+++ src/su/DailyAlgo/SWEA2805_농작물수확하기.java
@@ -0,0 +1,10 @@
+package src.su.DailyAlgo;
+
+public class SWEA2805_농작물수확하기 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
