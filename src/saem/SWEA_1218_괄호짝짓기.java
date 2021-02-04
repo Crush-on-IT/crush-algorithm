c1613cb55b3928d37ea68c5974833a6ac394bfb6
saeumi <saeumi@kakao.com>
2021-02-05 02:28:32 +0900

    SWEA_1218_괄호짝짓기

--- src/saem/SWEA_1218_괄호짝짓기.java
+++ src/saem/SWEA_1218_괄호짝짓기.java
@@ -9,7 +9,7 @@ public class SWEA_1218_괄호짝짓기 {
 //	peek() : Stack 최상단에 있는 top data 반환(Stack에서 꺼내진 않음)
 //	pop() : Stack에 있는 데이터 꺼내기
 //	push() : Stack에 데이터를 삽입
-//	isEmpty() : Stack 비어있는지 확인
+ 
 
  
 	
